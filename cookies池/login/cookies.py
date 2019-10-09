eea21d3c2f7ef1e7c3be398cd580c9aba5734725
zhangjie <wlh520z@163.com>
2019-10-10 00:50:04 +0800

    完善cookies池

--- cookies池/login/cookies.py
+++ cookies池/login/cookies.py
@@ -1,45 +1,62 @@
-import os
 import time
 from io import BytesIO
 from PIL import Image
-from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from os import listdir
+from os.path import abspath, dirname
 
-USERNAME = '15874295385'
-PASSWORD = 'fpdpvx119'
-
-TEMPLATES_FOLDER = 'templates/'
+TEMPLATES_FOLDER = dirname(abspath(__file__)) + '/templates/'
 
 
-class CrackWeiboSlide():
-    def __init__(self):
+class WeiboCookies():
+    def __init__(self, username, password, browser):
         self.url = 'https://passport.weibo.cn/signin/login?entry=mweibo&r=https://m.weibo.cn/'
-        self.browser = webdriver.Chrome()
+        self.browser = browser
         self.wait = WebDriverWait(self.browser, 20)
-        self.username = USERNAME
-        self.password = PASSWORD
-
-    def __del__(self):
-        self.browser.close()
-
+        self.username = username
+        self.password = password
+    
     def open(self):
         """
         打开网页输入用户名密码并点击
         :return: None
         """
+        self.browser.delete_all_cookies()
         self.browser.get(self.url)
         username = self.wait.until(EC.presence_of_element_located((By.ID, 'loginName')))
         password = self.wait.until(EC.presence_of_element_located((By.ID, 'loginPassword')))
         submit = self.wait.until(EC.element_to_be_clickable((By.ID, 'loginAction')))
-        username.send_keys(self.username)
-        password.send_keys(self.password)
+        username.send_keys(str(self.username, 'utf-8'))
+        password.send_keys(str(self.password, 'utf-8'))
+        time.sleep(1)
         submit.click()
-
+    
+    def password_error(self):
+        """
+        判断是否密码错误
+        :return:
+        """
+        try:
+            return WebDriverWait(self.browser, 5).until(
+                EC.text_to_be_present_in_element((By.ID, 'errorMsg'), '用户名或密码错误'))
+        except TimeoutException:
+            return False
+    
+    def login_successfully(self):
+        """
+        判断是否登录成功
+        :return:
+        """
+        try:
+            return bool(
+                WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'lite-iconf-profile'))))
+        except TimeoutException:
+            return False
+    
     def get_position(self):
         """
         获取验证码位置
@@ -56,7 +73,7 @@ class CrackWeiboSlide():
         top, bottom, left, right = location['y'], location['y'] + size['height'], location['x'], location['x'] + size[
             'width']
         return (top, bottom, left, right)
-
+    
     def get_screenshot(self):
         """
         获取网页截图
@@ -65,7 +82,7 @@ class CrackWeiboSlide():
         screenshot = self.browser.get_screenshot_as_png()
         screenshot = Image.open(BytesIO(screenshot))
         return screenshot
-
+    
     def get_image(self, name='captcha.png'):
         """
         获取验证码图片
@@ -75,9 +92,8 @@ class CrackWeiboSlide():
         print('验证码位置', top, bottom, left, right)
         screenshot = self.get_screenshot()
         captcha = screenshot.crop((left, top, right, bottom))
-        captcha.save(name)
         return captcha
-
+    
     def is_pixel_equal(self, image1, image2, x, y):
         """
         判断两个像素是否相同
@@ -96,7 +112,7 @@ class CrackWeiboSlide():
             return True
         else:
             return False
-
+    
     def same_image(self, image, template):
         """
         识别相似验证码
@@ -117,7 +133,7 @@ class CrackWeiboSlide():
             print('成功匹配')
             return True
         return False
-
+    
     def detect_image(self, image):
         """
         匹配图片
@@ -132,7 +148,7 @@ class CrackWeiboSlide():
                 numbers = [int(number) for number in list(template_name.split('.')[0])]
                 print('拖动顺序', numbers)
                 return numbers
-
+    
     def move(self, numbers):
         """
         根据顺序拖动
@@ -140,46 +156,77 @@ class CrackWeiboSlide():
         :return:
         """
         # 获得四个按点
-        circles = self.browser.find_elements_by_css_selector('.patt-wrap .patt-circ')
-        dx = dy = 0
-        for index in range(4):
-            circle = circles[numbers[index] - 1]
-            # 如果是第一次循环
-            if index == 0:
-                # 点击第一个按点
-                ActionChains(self.browser) \
-                    .move_to_element_with_offset(circle, circle.size['width'] / 2, circle.size['height'] / 2) \
-                    .click_and_hold().perform()
-            else:
-                # 小幅移动次数
-                times = 30
-                # 拖动
-                for i in range(times):
-                    ActionChains(self.browser).move_by_offset(dx / times, dy / times).perform()
-                    time.sleep(1 / times)
-            # 如果是最后一次循环
-            if index == 3:
-                # 松开鼠标
-                ActionChains(self.browser).release().perform()
-            else:
-                # 计算下一次偏移
-                dx = circles[numbers[index + 1] - 1].location['x'] - circle.location['x']
-                dy = circles[numbers[index + 1] - 1].location['y'] - circle.location['y']
-
-    def crack(self):
+        try:
+            circles = self.browser.find_elements_by_css_selector('.patt-wrap .patt-circ')
+            dx = dy = 0
+            for index in range(4):
+                circle = circles[numbers[index] - 1]
+                # 如果是第一次循环
+                if index == 0:
+                    # 点击第一个按点
+                    ActionChains(self.browser) \
+                        .move_to_element_with_offset(circle, circle.size['width'] / 2, circle.size['height'] / 2) \
+                        .click_and_hold().perform()
+                else:
+                    # 小幅移动次数
+                    times = 30
+                    # 拖动
+                    for i in range(times):
+                        ActionChains(self.browser).move_by_offset(dx / times, dy / times).perform()
+                        time.sleep(1 / times)
+                # 如果是最后一次循环
+                if index == 3:
+                    # 松开鼠标
+                    ActionChains(self.browser).release().perform()
+                else:
+                    # 计算下一次偏移
+                    dx = circles[numbers[index + 1] - 1].location['x'] - circle.location['x']
+                    dy = circles[numbers[index + 1] - 1].location['y'] - circle.location['y']
+        except:
+            return False
+    
+    def get_cookies(self):
+        """
+        获取Cookies
+        :return:
+        """
+        return self.browser.get_cookies()
+    
+    def main(self):
         """
         破解入口
         :return:
         """
         self.open()
+        if self.password_error():
+            return {
+                'status': 2,
+                'content': '用户名或密码错误'
+            }
+        # 如果不需要验证码直接登录成功
+        if self.login_successfully():
+            cookies = self.get_cookies()
+            return {
+                'status': 1,
+                'content': cookies
+            }
         # 获取验证码图片
         image = self.get_image('captcha.png')
         numbers = self.detect_image(image)
         self.move(numbers)
-        time.sleep(10)
-        print('识别结束')
+        if self.login_successfully():
+            cookies = self.get_cookies()
+            return {
+                'status': 1,
+                'content': cookies
+            }
+        else:
+            return {
+                'status': 3,
+                'content': '登录失败'
+            }
 
 
-if __name__ == '__main__':
-    crack = CrackWeiboSlide()
-    crack.crack()
+# if __name__ == '__main__':
+#     result = WeiboCookies('13258387521', '123456789zj').main()
+#     print(result)
